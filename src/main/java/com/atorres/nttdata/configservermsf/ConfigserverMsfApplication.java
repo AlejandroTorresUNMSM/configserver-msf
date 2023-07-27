248beb65b42095ca300a112e020df66538d55b6e
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-27 10:54:08 -0500

    dev: commit init

---
+++ src/main/java/com/atorres/nttdata/configservermsf/ConfigserverMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.configservermsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverMsfApplication.class, args);
+	}
+
+}
